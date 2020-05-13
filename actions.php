8af416f7d1115f5fef859dec535733a43f9d596f
ngangavic <ngangavictor10@gmail.com>
2020-05-13 11:44:49 +0300

    all action in one file

---
+++ actions.php
@@ -0,0 +1,14 @@
+<?php
+include "actions/Insert.php";
+include "actions/Select.php";
+include "actions/Delete.php";
+include "actions/Update.php";
+
+//insert
+$insert=new Insert();
+//delete
+$delete=new Delete();
+//select
+$select=new Select();
+//update
+$update=new Update();
