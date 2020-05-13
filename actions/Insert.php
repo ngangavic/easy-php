82251be5e1bd5bf97d14879809511876eceb0f44
ngangavic <ngangavictor10@gmail.com>
2020-05-13 10:37:32 +0300

    report and exit execution

--- actions/Insert.php
+++ actions/Insert.php
@@ -26,14 +26,14 @@ class Insert
     function insert_data()
     {
         if (empty($this->insertstatement) || empty($this->data)) {
-            echo 'SQL Statement or data not set';
+            exit('SQL Statement or data not set');
         } else {
             $stmt = $this->connection->prepare($this->insertstatement);
             $stmt->bind_param(str_repeat("s", count($this->data)), ...$this->data);
             if (!$stmt->execute()) {
-                echo $this->report = 'Insert Error';
+                $this->report = 'Insert Error';
             } else {
-                echo $this->report = 'Inserted';
+                $this->report = 'Inserted';
             }
         }
     }
